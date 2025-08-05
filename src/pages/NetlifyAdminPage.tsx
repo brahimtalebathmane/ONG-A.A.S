fcc27f87b5765d6ed0685f13c934830831e6f8bc
brahimtalebathmane <brahimtalebathmane@gmail.com>
2025-08-04 16:45:15 -1200

    Updated NetlifyAdminPage.tsx

--- src/pages/NetlifyAdminPage.tsx
+++ src/pages/NetlifyAdminPage.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 import { NetlifyPasswordSetup } from '../components/NetlifyPasswordSetup'
 
 export function NetlifyAdminPage() {
-  return <NetlifyPasswordSetup />
+  // Get inviteToken from URL params
+  const { inviteToken } = useParams<{ inviteToken?: string }>()
+
+  // Pass inviteToken to password setup component
+  return <NetlifyPasswordSetup inviteToken={inviteToken} />
 }
